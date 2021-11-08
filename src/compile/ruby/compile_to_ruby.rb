f5f3f90d7b949234075f4683dc7283ab311ce59d
Russell Jennings <violentpurr@gmail.com>
2021-11-08 10:59:19 -0500

    lambdas support

--- src/compile/ruby/compile_to_ruby.rb
+++ src/compile/ruby/compile_to_ruby.rb
@@ -10,11 +10,13 @@ class CompileToRuby
     self.new.rewrite(*args)
   end
   
+  # bookmark
   def rewrite(input, sheet_names, output)
     self.settable ||= lambda { |ref| false }
     self.defaults ||= []
     mapper = MapFormulaeToRuby.new
     mapper.sheet_names = sheet_names
+    output.puts ""
     input.each do |ref, ast|
       begin
         worksheet = ref.first.to_s
@@ -23,10 +25,10 @@ class CompileToRuby
         worksheet_c_name = mapper.sheet_names[worksheet] || worksheet.to_s
         name = worksheet_c_name.length > 0 ? "#{worksheet_c_name}_#{cell.downcase}" : cell.downcase
         if settable.call(ref)
-          output.puts "  attr_accessor :#{name} # Default: #{mapper.map(ast)}"
-          defaults << "    @#{name} = #{mapper.map(ast)}"
+          output.puts "  define_method(:#{name})".ljust(38) + "{ @#{name}.call } # Default: #{mapper.map(ast)}"
+          defaults << "    @#{name} = lambda { @#{name}_cache ||= #{mapper.map(ast)} }"  # rj added lambdas 
         else
-          output.puts "  def #{name}; @#{name} ||= #{mapper.map(ast)}; end"
+          output.puts "  define_method(:#{name})".ljust(38) + "{ @#{name}_cache ||= #{mapper.map(ast)} } "
         end
       rescue Exception => e
         puts "Exception at #{ref} => #{ast}"
