f7592a9ceb5d713d69399ec5290d6b24edc25b69
Russell Jennings <russell@wrapbook.com>
2021-11-11 15:11:22 -0500

    fixes ceiling() function rounding error

--- src/excel/excel_functions/ceiling.rb
+++ src/excel/excel_functions/ceiling.rb
@@ -1,23 +1,26 @@
+# frozen_string_literal: true
+
 module ExcelFunctions
-  
   def ceiling(number, multiple, mode = 0)
     return number if number.is_a?(Symbol)
     return multiple if multiple.is_a?(Symbol)
     return mode if mode.is_a?(Symbol)
+
     number = number_argument(number)
     multiple = number_argument(multiple)
     mode = number_argument(mode)
     return :value unless number.is_a?(Numeric)
     return :value unless multiple.is_a?(Numeric)
     return :value unless mode.is_a?(Numeric)
-    return 0 if multiple == 0
-    if mode == 0 || number > 0
+    return 0 if multiple.zero?
+
+    if mode.zero? || number.positive?
       whole, remainder = number.divmod(multiple)
-      num_steps = remainder > 0 ? whole + 1 : whole
+      # rj considering any super small values worth ignoring
+      num_steps = remainder > 0.00001 ? whole + 1 : whole
       num_steps * multiple
     else # Need to round negative away from zero
       -ceiling(-number, multiple, 0)
     end
   end
-  
 end
